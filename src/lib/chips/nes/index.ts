b9c8a14bd26abd61edb11b4ab771658331ba9b1e
freq-mod <32672779+freq-mod@users.noreply.github.com>
2026-08-21 14:34:43 +0200

    rudimentary NES playground

--- src/lib/chips/nes/index.ts
+++ src/lib/chips/nes/index.ts
@@ -1,9 +1,11 @@
 import { NES_CHIP as NES_CHIP_CORE } from './core';
 import NESInstrumentEditor from './NESInstrumentEditor.svelte';
+import NESPreviewRow from './NESPreviewRow.svelte';
 
 export const NES_CHIP = {
 	...NES_CHIP_CORE,
-	instrumentEditor: NESInstrumentEditor
+	instrumentEditor: NESInstrumentEditor,
+	previewRow: NESPreviewRow
 } as const;
 
 export { NESProcessor } from './processor';
@@ -11,4 +13,5 @@ export { NESConverter } from './adapter';
 export { NESFormatter } from './formatter';
 export { NESChipRenderer } from './renderer';
 export { NESInstrumentEditor };
+export { NESPreviewRow };
 export { NES_CHIP_SCHEMA } from './schema';
